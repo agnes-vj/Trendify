9eb815625542cd3ebe776cf1af0998dc601dfcfe
Agnes <133767448+agnes-vj@users.noreply.github.com>
2025-02-19 22:43:52 +0000

    Add OutfitCalendarEntry Repo

---
+++ WardrobeWebApp/Backend/Repositories/OutfitCalendarRepository.cs
@@ -0,0 +1,31 @@
+﻿using Backend.Models;
+
+namespace Backend.Repositories
+{
+    public class OutfitCalendarRepository: IOutfitCalendarRepository
+    {
+        private readonly WardrobeDBContext _dbContext;
+
+        public OutfitCalendarRepository(WardrobeDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void SaveOutfitCalendarEntry(OutfitCalendarEntry entry)
+        {
+            _dbContext.OutfitCalendarEntries.Add(entry);
+            _dbContext.SaveChangesAsync();
+        }
+
+        public List<OutfitCalendarEntry> GetOutfitCalendarEntries()
+        {
+            return _dbContext.OutfitCalendarEntries.ToList();
+        }
+    }
+
+    public interface IOutfitCalendarRepository
+    {
+        void SaveOutfitCalendarEntry(OutfitCalendarEntry entry);
+        List<OutfitCalendarEntry> GetOutfitCalendarEntries();
+    }
+}
